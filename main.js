c3049b91914f5983135fa194f0c6191fb83706cb
abdulrahman <abdulrahman102034@gmail.com>
2025-11-11 20:07:21 +0200

    add product features and solutions sections

---
+++ main.js
@@ -0,0 +1,17 @@
+let heading = document.querySelectorAll(".fade-up");
+
+checkVisibility = () => {
+  const triggerBottom = window.innerHeight * 0.85;
+
+  heading.forEach((el) => {
+    const boxTop = el.getBoundingClientRect().top;
+    console.log(boxTop);
+    if (boxTop < triggerBottom) {
+      el.classList.add("visible");
+    } else {
+      el.classList.remove("visible");
+    }
+  });
+};
+window.addEventListener("scroll", checkVisibility);
+checkVisibility();
